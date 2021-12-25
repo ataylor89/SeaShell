9a6e774043059f3c1168c5fcb44eaad5d6e0a4bc
Andrew <rolyata47@gmail.com>
2021-12-25 05:15:30 -0600

    Committing SeaShell code

---
+++ macbuild.sh
@@ -0,0 +1,12 @@
+jpackage --input target/ \
+  --name SeaShell \
+  --main-jar SeaShell.jar \
+  --main-class seashell.SeaShell \
+  --type dmg \
+  --icon "icons.icns" \
+  --app-version "1.0.0" \
+  --vendor "Andrew's software" \
+  --copyright "Copyright 2021" \
+  --mac-package-name "SeaShell" \
+  --verbose \
+  --java-options '--enable-preview'
